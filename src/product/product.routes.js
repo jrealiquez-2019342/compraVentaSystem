15ae1e53e7568bdd9a8f2cf3807152837574c322
jrealiquez-2019342 <jrealiquez-2019342@kinal.edu.gt>
2024-02-27 23:08:39 -0600

    Se ordenaron las rutas public-private

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,15 +1,55 @@
 import { Router } from 'express'
 import { test, save, update, deleteP, get, getProduct, productOut, searchProduct } from './product.controller.js'
+import { validateJwt, isAdmin, isClient } from './../middlewares/validate-jwt.js';
 
 const api = Router();
 
+//rutas publicas
 api.get('/test', test);
-api.get('/get', get);
+
+
+//================//
+//rutas privadas //
+//==============//
+
+
+//=========================//
+//rutas privadas -- ADMIN //
+//=======================//
+
+//Agregar productos a la base de datos
+api.post('/save', [validateJwt, isAdmin], save);
+
+//Visualizar producto individual
 api.get('/getProduct/:id', getProduct);
-api.get('/getProductOut', productOut);
-api.post('/searchProduct', searchProduct);
-api.post('/save', save);
+
+//visualizar catalogo completo
+api.get('/get', get);
+
+//editar detalles especificos de un producto
 api.put('/update/:id', update);
+
+//productos agotados
+api.get('/getProductOut', [validateJwt, isAdmin], productOut);
+
+//productos mas vendidos
+
+//eliminar un producto
 api.delete('/delete/:id', deleteP);
+
+//=========================//
+//rutas privadas -- CLIENT //
+//=========================//
+
+//catalogo productos mas vendidos
+
+//buscar productos por nombre
+api.post('/searchProduct', searchProduct);
+
+//explorar las categorias existentes
+
+// productos segun la categoria proporcionada
+
+
 
 export default api;
