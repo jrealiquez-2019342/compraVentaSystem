1dc0bb6863d2f1a8f9dfeba7d71cda4c347c16c7
jrealiquez-2019342 <jrealiquez-2019342@kinal.edu.gt>
2024-02-28 00:13:57 -0600

    Se clasificaron las rutas publicas-privadas

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,12 +1,35 @@
 import { Router } from 'express';
 import { test, save, get, deleteC, updateC} from './category.controller.js';
+import { validateJwt, isAdmin, isClient } from './../middlewares/validate-jwt.js';
 
 const api = Router();
 
+/*========================== */
+/*       Rutas Publicas      */
+/*========================== */
+
+//test
 api.get('/test', test);
-api.get('/get', get);
-api.post('/save', save);
-api.delete('/delete/:id', deleteC);
-api.put('/update/:id', updateC)
+
+/*============================ */
+/*Rutas Privadas - Compartidas */
+/*============================ */
+
+//Visualizar todas las categorias
+api.get('/get', [validateJwt], get);
+
+/*========================== */
+/*  Rutas Privadas - Admin   */
+/*========================== */
+
+//Agregar categoria
+api.post('/save', [validateJwt, isAdmin], save);
+
+//Editar categoria
+api.put('/update/:id', [validateJwt, isAdmin], updateC)
+
+// eliminar categoria
+api.delete('/delete/:id', [validateJwt, isAdmin], deleteC);
+
 
 export default api;
